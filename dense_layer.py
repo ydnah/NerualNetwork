cb30ca2147ef147ca67b1506f7c94f4fe24d2c55
Sam <98478965+ydnah@users.noreply.github.com>
2023-11-21 16:25:37 +0000

    Added test and train add from MNIST

--- dense_layer.py
+++ dense_layer.py
@@ -133,6 +133,7 @@ class Categorical_Cross_Entropy(Loss):
         # Normalize the gradient to make the sum's magnitude invarient to the number of samples
         self.dinputs = self.dinputs / samples
 
+# Creatomg class for faster back prop
 class Activation_Softmax_Loss_CategoricalCrossentropy():
     def __init__(self):
         self.activation = Softmax()
@@ -263,17 +264,29 @@ class Optimizer_Adam():
     def post_update_params(self):
         self.iterations += 1
         
-X, y = spiral_data(samples=1000, classes = 3)
+#X, y = spiral_data(samples=1000, classes = 3)
 
-dense1 = Dense_Layer(2, 512)
+import pandas as pd
+
+train = pd.read_csv('train.csv')
+
+X = train.iloc[:, 1:].values
+X = (X.reshape(X.shape[0], -1).astype(np.float32) - 127.5 / 127.5)
+y = train.iloc[:, 0].values
+
+
+dense1 = Dense_Layer(X.shape[1], 512)
 activation1 = ReLU_Activation()
 dropout1 = Dropout_Layer(0.1)
-dense2 = Dense_Layer(512, 3)
+dense2 = Dense_Layer(512, 256)
+activation2 = ReLU_Activation()
+dropout2 = Dropout_Layer(0.1)
+dense3 = Dense_Layer(256, 10)
 loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
-optimizer = Optimizer_Adam(learning_rate=0.05, decay=5e-5)
+optimizer = Optimizer_Adam(decay=5e-5)
 
 # Train in loop
-for epoch in range(10001):
+for epoch in range(10):
     # Perform a forward pass of our training data through this layer
     dense1.forward(X)
     # Perform a forward pass through activation function
@@ -283,23 +296,29 @@ for epoch in range(10001):
     # Perform a forward pass through second Dense layer
     # takes outputs of activation function of first layer as inputs
     dense2.forward(dropout1.output)
+    activation2.forward(dense2.output)
     # Perform a forward pass through the activation/loss function
     # takes the output of second dense layer here and returns loss
-    loss = loss_activation.forward(dense2.output, y)
+    dropout2.forward(activation2.output)
+    dense3.forward(dropout2.output)
+    loss = loss_activation.forward(dense3.output, y)
     # Calculate accuracy from output of activation2 and targets
     # calculate values along first axis
     predictions = np.argmax(loss_activation.output, axis=1)
     if len(y.shape) == 2:
         y = np.argmax(y, axis=1)
     accuracy = np.mean(predictions==y)
-    if not epoch % 1000:
-        print(f'epoch: {epoch}, ' +
+    #if not epoch % 10:
+    print(f'epoch: {epoch}, ' +
         f'acc: {accuracy:.3f}, ' +
         f'loss: {loss:.3f}' +
         f'lr : {optimizer.current_learning_rate}')
     # Backward pass
     loss_activation.backward(loss_activation.output, y)
-    dense2.backward(loss_activation.dinputs)
+    dense3.backward(loss_activation.dinputs)
+    dropout2.backward(dense3.dinputs)
+    activation2.backward(dropout2.dinputs)
+    dense2.backward(activation2.dinputs)
     dropout1.backward(dense2.dinputs)
     activation1.backward(dropout1.dinputs)
     dense1.backward(activation1.dinputs)
@@ -309,12 +328,21 @@ for epoch in range(10001):
     optimizer.update_params(dense2)
     optimizer.post_update_params()
 
-X_test, y_test = spiral_data(samples=100, classes=3)
+#X_test, y_test = spiral_data(samples=100, classes=3)
+
+test = pd.read_csv('test.csv')
+
+X_test = train.iloc[:, 1:].values
+X_test = (X.reshape(X.shape[0], -1).astype(np.float32) - 127.5 / 127.5)
+y_test = train.iloc[:, 0].values
 
 dense1.forward(X_test)
 activation1.forward(dense1.output)
 dense2.forward(activation1.output)
-loss = loss_activation.forward(dense2.output, y_test)
+activation2.forward(dense2.output)
+dense3.forward(dropout2.output)
+
+loss = loss_activation.forward(dense3.output, y_test)
 
 predictions = np.argmax(loss_activation.output, axis=1)
 if len(y_test.shape) == 2:
